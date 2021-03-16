188735ec1e8602f96bcb56bdc0ebe79a719dde08
BeeSwapFinance <beeswapfinance@gmail.com>
2021-03-16 22:08:10 +0100

    New Icons.

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -17,3 +17,5 @@ export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
 export { default as AuditIcon } from "./Audit";
+export { default as BurningIcon } from "./Burning";
+export { default as RoadmapIcon } from "./Roadmap";
